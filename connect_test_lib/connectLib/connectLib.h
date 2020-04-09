a44367ef606a666bacf131bb5d264f09c0b5a638
jjsanderson <lists@quernstone.com>
2020-04-09 12:01:32 +0100

    First stub of connectLib

---
+++ connect_test_lib/connectLib/connectLib.h
@@ -0,0 +1,24 @@
+/**
+ * Connect.h - Library for Connect devices
+ * Created by Jonathan Sanderson, April 2020
+ * Northumbria University 
+ * See https://nustem.uk/connect
+ * TODO: License
+*/
+
+#ifndef Connect_h
+#define Connect_h
+
+#include "Kniwwelino.h"
+
+class Connect
+{
+    public:
+        Connect(int pin); //root object, needs variable stubs
+        void func1();
+        void func2();
+    private:
+        int _pin;
+};
+
+#endif
