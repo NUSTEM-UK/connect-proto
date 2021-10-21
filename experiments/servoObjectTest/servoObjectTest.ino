d8f5b206cf966209b176921a6d6f5ca99c064ae1
jjsanderson <jonathan@quernstone.com>
2021-10-21 14:33:38 +0100

    ServoObjectTest moved to ConnectServo library

--- experiments/servoObjectTest/servoObjectTest.ino
+++ experiments/servoObjectTest/servoObjectTest.ino
@@ -3,10 +3,72 @@
 
 ConnectServo servo1;
 
+typedef struct {
+    String targetDevice;
+    GenericFP call;
+    int queueVar1;
+    float queueVar2;
+    char * queueVar3;
+    int startPos;
+    int endPos;
+    const char * animationType;
+    float animationDuration;
+} deviceQueueItem;
+
+deviceQueueItem tempDeviceQueueItem;
+String TargetDevice = "Servo1";
+
+
+// Give ourselves some placeholder things to work with
+ServoQueueItem myItem;
+void myFunction1(void *skip, ...);
+void myFunction2(void *skip, ...);
+int value = 12;
+int speed = 30;
+
 void setup() {
+    Serial.begin(115200);
+    Serial.println("Starting...");
     servo1.attach(D5);
     servo1.write(0);
     delay(500);
+    tempDeviceQueueItem = {TargetDevice, &myFunction1, 3, 0.0, "", 0, 0, "", 0.0};
+
+    myItem.call = &myFunction1;
+    myItem.param1 = value;
+    myItem.animationType = "EASE_CUBIC_IN_OUT";
+    myItem.servoSpeed = 100;
+    // myItem = {&myFunction1, value, 'EASE_CUBIC_IN_OUT', speed};
+    servo1.enqueue(myItem);
+    myItem.call = &myFunction2;
+    delay(50);
+    myItem.param1 = 1;
+    myItem.animationType = "EASE_BACK_BOUNCING";
+    myItem.servoSpeed = 200;
+
+    // myItem = {&myFunction2, 1, 'EASE_BACK_BOUNCING', 200};
+    servo1.enqueue(myItem);
+
+    myItem = servo1.dequeue();
+    Serial.print("Dequeued: ");
+    // Serial.print(myItem.call);
+    // Serial.print(" ");
+    Serial.print(myItem.param1);
+    Serial.print(" ");
+    Serial.print(myItem.animationType);
+    Serial.print(" ");
+    Serial.println(myItem.servoSpeed);
+
+    myItem = servo1.dequeue();
+    Serial.print("Dequeued: ");
+    // Serial.print(myItem.call);
+    // Serial.print(" ");
+    Serial.print(myItem.param1);
+    Serial.print(" ");
+    Serial.print(myItem.animationType);
+    Serial.print(" ");
+    Serial.println(myItem.servoSpeed);
+
 }
 
 void loop() {
@@ -14,9 +76,30 @@ void loop() {
     while (servo1.isMovingAndCallYield() ) {
         delay(10);
     }
-    servo1.startEaseTo(0, 200);
+    servo1.write(0);
     while (servo1.isMovingAndCallYield() ) {
         delay(10);
     }
 }
 
+void myFunction1(void *skip, ...)
+{
+    va_list args;
+    va_start(args, skip);
+    int foo1 = va_arg(args, int);
+    Serial.print("myFunction1 = ");
+    Serial.println(foo1);
+}
+
+void myFunction2(void *skip, ...)
+{
+    va_list args;
+    va_start(args, skip);
+    int foo1 = va_arg(args, int);
+    float foo2 = (float)va_arg(args, double);
+
+    Serial.print("myFunction2 = ");
+    Serial.print(foo1);
+    Serial.print(", ");
+    Serial.println(foo2);
+}
